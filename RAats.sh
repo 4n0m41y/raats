3f534cf674de94ece7b89fa17b63b2cb4257f109
4n0m41y <4n0m41y@users.noreply.github.com>
2017-04-06 09:51:29 -0500

    Add files via upload

---
+++ RAats.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+# ---------------------------------------------------
+# Author: 4n0m41y
+# 7 April 2017
+# ---------------------------------------------------
+#GNU GENERAL PUBLIC LICENSE
+#Version 3 - 29 June 2007
+#Copyright © 2007 Free Software #Foundation, Inc.
+#<http://fsf.org/>
+# ---------------------------------------------------
+# A simple bash script to circumvent 
+# pre installed NSA malware on 
+# Android phones. Feel Free to modify. 
+# Direct all questions and issues to github 
+# ---------------------------------------------------
+echo "Run this as root"
+ echo "Your gay if you don't use Debian"          
+cd /data/system  
+          echo "Looking For Government Agents" 
+          sleep 4 & PID=$!
+
+echo "WORKING, PLEASE BE PATIENT"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+       sleep 1
+ if
+          ls | grep ndebugsocket
+          then echo "Romoving PHat RAat"
+          rm -Rf ndebugsocket
+          
+ fi
+        sleep 1 
+           sleep 4 & PID=$!
+
+echo "Looking For Other Spies"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+if
+          ls | grep recent_images
+          then echo "Removing other RAats"          
+          rm -Rf recent_images
+          
+ fi
+        sleep 1 
+           sleep 4 & PID=$!
+
+echo "Searching Through Hillarys Emails"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+ if
+          ls | grep recent_tasks
+          then echo "Removing other RAats" 
+          rm -Rf recent_tasks
+          
+ fi 
+      cd ..          
+        sleep 1 
+           sleep 4 & PID=$!
+
+echo "Locating Osama Bin Laden"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+ if
+          ls | grep cam_socket
+          then echo "Removing other RAats" 
+          rm -Rf cam_socket
+          
+ fi
+         sleep 1 
+           sleep 4 & PID=$!
+
+echo "Accessing The NSA Mainframe"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+ if
+          ls | grep cam_socket1
+          then echo "Removing other RAats" 
+          rm -Rf cam_socket1
+          
+ fi  
+        sleep 1 
+           sleep 4 & PID=$!
+
+echo "Downloading Top Secret Information"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+ if
+          ls | grep cam_socket2
+          then echo "Removing other RAats" 
+          rm -Rf cam_socket2
+          
+ fi
+         sleep 1 
+           sleep 4 & PID=$!
+
+echo "Stealing Gay Porn"
+printf "|"
+while kill -0 $PID 2> /dev/null; do 
+    printf  "*"
+    sleep 0.3
+done
+printf "|"
+ if
+          ls | grep cam_socket3
+          then echo "Removing other RAats" 
+          rm -Rf cam_socket3
+          
+ fi          
+          echo "Yippee ki-yay, Motherfuckers!"
+          
+          echo "!!!THIS ONLY WORKS UNTIL REBOOT!!!"          
+#done
